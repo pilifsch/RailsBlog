a7d1d1e8053917943287b1c2d02173376eb7aa18
pilif <toor@riseup.net>
2016-04-16 11:32:45 +0200

    user posts relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
 
 	validates :user, presence: true
 	validates :user, uniqueness: true
+	belongs_to :user
 	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416083251) do
+ActiveRecord::Schema.define(version: 20160416092619) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.string   "user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
